348f365d72437c5dbbc9e27bfeac7b62de548e06
hemanthkumarbandaru <hemanthkumarbandaru469@gmail.com>
2024-12-29 18:07:59 +0530

    Removed subfolders from the repository

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask,request,render_template,redirect,url_for,flash,session
+from flask import Flask,request,render_template,redirect,url_for,flash,session,send_file
 from otp import genotp
 from cmail import sendmail
 from stoken import encode,decode
 from flask_session import Session
 import mysql.connector
+from io import  BytesIO
+import flask_excel as excel
+import re
 app=Flask(__name__)
 app.secret_key='codegnan@2018'
 app.config['SESSION_TYPE']='filesystem'
+app.secret_key='hemanth#123'
 Session(app)
 mydb=mysql.connector.connect(host='localhost',user='root',password='admin',db='snmproject')
-
+excel.init_excel(app)
 @app.route('/')
 def home():
     return render_template('welcome.html')
@@ -93,105 +97,243 @@ def dashboard():
     return render_template('dashboard.html')
 @app.route("/addnotes",methods=['POST','GET'])
 def addnotes():
-    if request.method=="POST":
-        title=request.form["title"]
-        desc=request.form["desc"]
+    if session.get('user'):
+        if request.method=="POST":
+            title=request.form["title"]
+            desc=request.form["desc"]
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute("select user_id from users where useremail=%s",[session.get('user')])
+            id=cursor.fetchone()
+            if id:
+                try:
+                    cursor.execute('insert into notes(title,n_description,user_id) values(%s,%s,%s)',[title,desc,id[0]])
+                    mydb.commit()
+                    cursor.close()
+                except mysql.connector.errors.IntegrityError:
+                    flash("Duplicate Title Entry")
+                    return redirect(url_for('dashboard'))
+                else:
+                    flash("Notes add successfully")
+                    return redirect(url_for('dashboard'))
+            else:
+                return "Something went wrong"
+        return render_template("addnotes.html")
+    else:
+        return redirect(url_for('login'))
+@app.route('/viewallnotes')
+def viewallnotes():
+    if session.get('user'):
+        try:
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute("select user_id from users where useremail=%s",[session.get('user')])
+            uid=cursor.fetchone()
+            cursor.execute('select n_id,title,create_at from notes where user_id=%s',[uid[0]])
+            ndata=cursor.fetchall()
+            print(ndata)
+        except Exception as e:
+            print(e)
+            return redirect(url_for("dashboard"))
+        else:
+            return render_template("viewallnotes.html",ndata=ndata)
+    else:
+        return redirect(url_for('login'))
+@app.route('/viewnotes/<nid>')
+def viewnotes(nid):
+    if session.get('user'):
+
+        try:
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute("select * from notes where n_id=%s",[nid])
+            ndata=cursor.fetchone()
+        except Exception as e:
+            print(e)
+            return redirect(url_for("viewallnotes"))
+        else:
+            return render_template("viewnotes.html",ndata=ndata)
+    else:
+        return redirect(url_for('login'))
+@app.route("/updatenotes/<nid>",methods=["POST","GET"])
+def updatenotes(nid):
+    if session.get('user'):
+
         cursor=mydb.cursor(buffered=True)
-        cursor.execute("select user_id from users where useremail=%s",[session.get('user')])
-        id=cursor.fetchone()
-        if id:
+        cursor.execute("select * from notes where n_id=%s",[nid])
+        ndata=cursor.fetchone()
+        if request.method=="POST":
             try:
-                cursor.execute('insert into notes(title,n_description,user_id) values(%s,%s,%s)',[title,desc,id[0]])
+                title=request.form["title"]
+                desc=request.form["desc"]
+                cursor=mydb.cursor(buffered=True)
+                cursor.execute('update notes set title=%s,n_description=%s where n_id=%s',[title,desc,nid])
                 mydb.commit()
                 cursor.close()
-            except mysql.connector.errors.IntegrityError:
-                flash("Duplicate Title Entry")
+            except Exception as e:
+                print(e)
+            else:
+                flash("Notes updates successfully")
+                return redirect(url_for('dashboard'))
+        return render_template("updatenotes.html",ndata=ndata)
+    else:
+        return redirect(url_for('login'))
+    
+@app.route("/deletenotes/<nid>",methods=["POST","GET"])
+def deletenotes(nid):
+    if session.get('user'):
+        try:
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute("delete from notes where n_id=%s",[nid])
+            mydb.commit()
+            cursor.close()
+        except Exception as e:
+            print(e)
+        else:
+            flash("Deleted successfully")
+            return redirect(url_for("viewallnotes"))
+    else:
+        return redirect(url_for('login'))
+@app.route('/uploadfile',methods=['GET','POST'])
+def uploadfile():
+    if session.get('user'):
+        print(session.get('user'))
+        if request.method=='POST':
+            filedata=request.files['file']
+            fname=filedata.filename
+            fdata=filedata.read()
+            try:
+                cursor=mydb.cursor(buffered=True)
+                cursor.execute('select user_id from users where useremail=%s',[session.get('user')])
+                uid=cursor.fetchone()
+                cursor.execute("insert into filedata(filename,fdata,added_by) values(%s,%s,%s)",[fname,fdata,uid[0]])
+                mydb.commit()
+            except Exception as e:
+                print(e)
+                flash('Could not upload file')
                 return redirect(url_for('dashboard'))
             else:
-                flash("Notes add successfully")
+                flash('file uploaded successfully')
                 return redirect(url_for('dashboard'))
+        return render_template('upload.html')
+    else:
+        return redirect(url_for('login'))
+@app.route('/viewallfiles')
+def viewallfiles():
+    if session.get('user'):
+        try:
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute("select user_id from users where useremail=%s",[session.get('user')])
+            uid=cursor.fetchone()
+            cursor.execute("select fid,filename,created_at from filedata where added_by=%s",[uid[0]])
+            filedata=cursor.fetchall()
+        except Exception as e:
+            print(e)
+            flash('No data Found')
+            return redirect(url_for('dashboard'))
         else:
-            return "Something went wrong"
-    return render_template("addnotes.html")
-@app.route('/viewallnotes',methods=['POST',"GET"])
-# def viewallnotes():
-#     try:
-#         cursor=mydb.cursor(buffered=True)
-#         cursor.execute("select user_id from users where useremail=%s",[session.get('user')])
-#         uid=cursor.fetchone()
-#         cursor.execute('select n_id,title,create_at from notes where user_id=%s',[uid[0]])
-#         result=cursor.fetchall()
-#         print(result)
-#     except Exception as e:
-#         print(e)
-#         return redirect(url_for("dashboard"))
-#     else:
-#         return render_template("viewallnotes.html",result=result)
-# @app.route('/viewnotes/<uid>')
-# def viewnotes(uid):
-#     try:
-#         cursor=mydb.cursor(buffered=True)
-#         cursor.execute("select * from notes where n_id=%s",[uid])
-#         notes=cursor.fetchone()
-#     except Exception as e:
-#         print(e)
-#         return redirect(url_for("viewallnotes"))
-#     else:
-#         return render_template("viewnotes.html",notes=notes)
-# @app.route("/updatenotes/<uid>",methods=["POST","GET"])
-# def updatenotes(uid):
-#     cursor=mydb.cursor(buffered=True)
-#     cursor.execute("select * from notes where n_id=%s",[uid])
-#     notes=cursor.fetchone()
-#     if request.method=="POST":
-#         try:
-#             title=request.form["title"]
-#             desc=request.form["desc"]
-#             cursor=mydb.cursor(buffered=True)
-#             cursor.execute('update notes set title=%s,n_description=%s where n_id=%s',[title,desc,uid])
-#             mydb.commit()
-#             cursor.close()
-#         except Exception as e:
-#             print(e)
-#         else:
-#             flash("Notes updates successfully")
-#             return redirect(url_for('dashboard'))
-#     return render_template("updatenotes.html",notes=notes)
-# @app.route("/deletenotes/<uid>",methods=["POST","GET"])
-# def deletenotes(uid):
-#     try:
-#         cursor=mydb.cursor(buffered=True)
-#         cursor.execute("delete from notes where n_id=%s",[uid])
-#         mydb.commit()
-#         cursor.close()
-#     except Exception as e:
-#         print(e)
-#     else:
-#         flash("Deleted successfully")
-#         return redirect(url_for("viewallnotes"))
-# @app.route('/search',methods=['GET','POST'])
-# def search():
-#     if session.get('user'):
-#         try:
-#             if request.method=='POST':
-#                 sdata=request.form['sname'] #''python',s22
-#                 strg=['A-Za-z0-9']
-#                 pattern=re.compile(f'^{strg}',re.IGNORECASE)
-#                 if (pattern.match(sdata)):
-#                     cursor=mydb.cursor(buffered=True)
-#                     cursor.execute('select * from notes where nid like %s or title like %s or ndescription like %s or create_at like %s',[sdata+'%',sdata+'%',sdata+'%',sdata+'%'])
-#                     sdata=cursor.fetchall()
-#                     cursor.close()
-#                     return render_template('dashboard.html',sdata=sdata)
-#                 else:
-#                     flash('no data found')
-#                     return redirect(url_for('dashboard'))
-#             else:
-#                 return redirect(url_for('dashboard'))
-#         except Exception as e:
-#             print(e)
-#             flash('Cantfind anything')
-#             return redirect(url_for('login'))
-#     else:
-#         return redirect(url_for('login'))
-app.run(use_realoader=True)
+            return render_template('allfiles.html',filedata=filedata)
+    else:
+        return redirect(url_for('login'))
+@app.route('/viewfile/<fid>')
+def viewfile(fid):
+    if session.get('user'):
+        try:
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute('select filename,fdata from filedata where fid=%s',[fid])
+            fdata=cursor.fetchone()
+            bytes_data=BytesIO(fdata[1])
+            return send_file(bytes_data,download_name=fdata[0],as_attachment=False)
+        except Exception as e:
+            print(e)
+            flash("couldn't not open file")
+            return redirect(url_for("dashboard"))
+@app.route('/downloadfile/<fid>')
+def downloadfile(fid):
+        if session.get('user'):
+            try:
+                cursor=mydb.cursor(buffered=True)
+                cursor.execute('select filename,fdata from filedata where fid=%s',[fid])
+                fdata=cursor.fetchone()
+                bytes_data=BytesIO(fdata[1])
+                return send_file(bytes_data,download_name=fdata[0],as_attachment=True)
+            except Exception as e:
+                print(e)
+                flash("couldn't not open file")
+                return redirect(url_for("dashboard"))
+        else:
+            return redirect(url_for('login'))
+@app.route('/deletefile/<fid>')
+def deletefile(fid):
+    if session.get('user'):
+        try:
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute("delete from filedata where fid=%s",[fid])
+            mydb.commit()
+            cursor.close()
+        except Exception as e:
+            print(e)
+            flash('something went wrong')
+            return redirect(url_for('viewallfiles'))
+        
+        else:
+            flash("succefully deleted")
+            return redirect(url_for('viewallfiles'))
+    else:
+        return redirect(url_for('login'))
+@app.route('/getexceldata')
+def getexceldata():
+    if session.get('user'):
+        try:
+            cursor=mydb.cursor(buffered=True)
+            cursor.execute('select user_id from users where useremail=%s',[session.get('user')])
+            uid=cursor.fetchone() #(1,)
+            cursor.execute('select n_id, title, n_description, create_at from notes where user_id=%s',[uid[0]])
+            ndata=cursor.fetchall()   #[(1, 'python','2024-12-16 11:14:25,),(2,'python','2024-12-16 11:14:53,)]
+        except Exception as e:
+            print(e)
+            flash('No data found')
+            return redirect(url_for('dashboard'))
+        else:
+            array_data=[list(i) for i in ndata]
+            columns=['Notesid','Title','Content','Created_time']
+            array_data.insert(0,columns)
+            #[['Notesid','Title','Content','Created_time'],[1, 'python','2024-12-16 11:14:25,],[2,'python','2024-12-16 11:14:53,]]
+            return excel.make_response_from_array(array_data,'xlsx',filename='notesdata')
+    else:
+        return redirect(url_for('login'))
+@app.route('/logout')
+def logout():
+    print(session.get('user'))
+    if session.get('user'):
+        session.pop('user')
+        print('pop',session.get('user'))
+        return redirect(url_for('home'))
+    else:
+        return redirect(url_for('login'))
+@app.route('/search',methods=['GET','POST'])
+def search():
+    if session.get('user'):
+        try:
+            if request.method=='POST':
+                sdata=request.form['sname']
+                strg=['A-Za-z0-9']
+                pattern=re.compile(f'^{strg}',re.IGNORECASE)
+                if(pattern.match(sdata)):
+                    cursor=mydb.cursor(buffered=True)
+                    cursor.execute('select * from notes where n_id like %s or title like %s or n_description like %s or create_at like %s',
+                    s[data+'%',sdata+'%',sdata+'%',sdata+'%'])
+                    sdata=cursor.fetchall()
+                    cursor.close()
+                    return render_template('dashboard.html',sdata=sdata)
+                else:
+                    flash('No Data Found')
+                    return redirect(url_for('dashboard'))
+            else:
+                return redirect(url_for('dashboard'))
+        except Exception as e:
+            print(e)
+            flash('Cant find anything')
+            return redirect(url_for('dashboard'))
+    else:
+        return redirect(url_for('login'))
+app.run(use_reloader=True, debug=True)
+        
+
